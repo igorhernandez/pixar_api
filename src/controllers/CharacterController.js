ffd5c8ab99c3ba736992a79fce0865edd895b135
Igor Hernandez <igorhernandez@MacbookProIgor.local>
2021-01-06 15:30:21 -0300

    feat: finalizado tratativa de dados

--- src/controllers/CharacterController.js
+++ src/controllers/CharacterController.js
@@ -1,8 +1,87 @@
 const Character = require("../models/Character");
+const { JSDOM } = require("jsdom");
+const axios = require("axios");
 
 module.exports = {
-  async index(res) {
-    const characters = await Character.find();
-    return res.json(characters);
+  async index(req, res) {
+    // const charactersNames = require("../charactersNames");
+
+    // const takeCharacterData = async (characterNameData) => {
+    //   const characterName = characterNameData.replace(" ", "_");
+    //   try {
+    //     const { data } = await axios.get(
+    //       `https://disney.fandom.com/pt-br/wiki/${characterName}`
+    //     );
+    //     const dom = new JSDOM(data);
+    //     const { document } = dom.window;
+
+    //     const name = document.getElementById("firstHeading").innerHTML || "";
+    //     const biographElement = document.querySelector(".mw-parser-output");
+    //     const bioPharagraphs = biographElement.getElementsByTagName("p");
+    //     console.log({ bioPharagraphs: bioPharagraphs.length, name });
+    //     const bio =
+    //       bioPharagraphs.length <= 5
+    //         ? bioPharagraphs[0].innerHTML
+    //         : bioPharagraphs[1].innerHTML;
+    //     const avatar_url =
+    //       document.querySelector(
+    //         "#WikiaMainContentContainer #mw-content-text #infoboxinternal .image > img"
+    //       ).dataset.src || "";
+
+    //     const character = { name, bio, avatar_url };
+
+    //     return character;
+    //   } catch (error) {
+    //     return res.send(error);
+    //   }
+
+    // };
+
+    // const handleAllNamesCharacters = async () => {
+    //   const characters = charactersNames.map((nameCharacter) =>
+    //     takeCharacterData(nameCharacter)
+    //   );
+    //   return Promise.all(characters);
+    // };
+
+    // return handleAllNamesCharacters().then((characters) =>
+    //   res.send(characters)
+    // );
+
+    const charactersArray = require("../charactersArray");
+
+    const getCharacterPage = async (characterName) => {
+      const characterNameFormated = characterName.replace(" ", "_");
+      const { data } = await axios.get(
+        `https://disney.fandom.com/pt-br/wiki/${characterNameFormated}`
+      );
+      return data;
+    };
+
+    const changeAvatarUrl = async () => {
+      const newCharacters = charactersArray.map(async (character) => {
+        const { name, avatar_url } = character;
+        if (
+          avatar_url ===
+          "data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D"
+        ) {
+          const characterPageData = await getCharacterPage(name);
+
+          const dom = new JSDOM(characterPageData);
+          const { document } = dom.window;
+          const imgElement = document.querySelector(
+            "#infoboxinternal .image > img"
+          );
+          const avatarImgGet = imgElement ? imgElement.dataset.src : "";
+          character.avatar_url = avatarImgGet;
+          return character;
+        } else {
+          return character;
+        }
+      });
+      return Promise.all(newCharacters);
+    };
+
+    return changeAvatarUrl().then((newCharacters) => res.send(newCharacters));
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,62 +1,7 @@
-// const express = require("express");
-// const routes = require("./routes");
-// const app = express();
+const express = require("express");
+const routes = require("./routes");
+const app = express();
 
-// app.use(express.json());
-// app.use(routes);
-// app.listen(3334);
-
-const http = require("http");
-const { JSDOM } = require("jsdom");
-const axios = require("axios");
-
-const port = 5555;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
-
-server.listen(port, () => {
-  console.log(`Server running at PORT:${port}/`);
-});
-
-const charactersNames = require("./charactersNameShort");
-
-const takeCharacterData = async (characterNameData) => {
-  const characterName = characterNameData.replace(" ", "_");
-  try {
-    const { data } = await axios.get(
-      `https://disney.fandom.com/pt-br/wiki/${characterName}`
-    );
-    const dom = new JSDOM(data, {
-      runScripts: "dangerously",
-      resources: "usable",
-    });
-    const { document } = dom.window;
-    const name = document.getElementById("firstHeading").innerHTML;
-    const biographElement = document.querySelector(".mw-parser-output");
-    const bioPharagraphs = biographElement.getElementsByTagName("p");
-    console.log({ bioPharagraphs: bioPharagraphs.length, name });
-    const bio =
-      bioPharagraphs.length <= 3
-        ? bioPharagraphs[0].innerHTML
-        : bioPharagraphs[1].innerHTML;
-    const avatar_url = document.querySelector(".image > img").src;
-
-    const character = { name, bio, avatar_url };
-    return character;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const handleAllNamesCharacters = async () => {
-  const characters = charactersNames.map((nameCharacter) =>
-    takeCharacterData(nameCharacter)
-  );
-  return Promise.all(characters);
-};
-
-handleAllNamesCharacters().then((characters) => console.log(characters));
+app.use(express.json());
+app.use(routes);
+app.listen(3334);
